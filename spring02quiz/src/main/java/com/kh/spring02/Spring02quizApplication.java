b4a5d70558f9daab242986f55c924c98c72c9ac6
wndudwhdka <wndudwhdka@naver.com>
2023-01-30 13:58:21 +0900

    spring-web, spring-devtools dependencies

---
+++ spring02quiz/src/main/java/com/kh/spring02/Spring02quizApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring02quizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring02quizApplication.class, args);
+	}
+
+}
