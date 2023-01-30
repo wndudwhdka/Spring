b1203fa52e9fca7eaf5ad8af3c8f038a69a1afaf
wndudwhdka <wndudwhdka@naver.com>
2023-01-30 13:55:39 +0900

    spring01 first

---
+++ spring01basic/src/main/java/com/kh/spring01/Spring01basicApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring01basicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring01basicApplication.class, args);
+	}
+
+}
---
+++ spring01basic/src/test/java/com/kh/spring01/Spring01basicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01basicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
