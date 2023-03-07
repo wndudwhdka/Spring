46e4abded89cd16d3155a2df9ab92caf64a397c2
wndudwhdka <wndudwhdka@naver.com>
2023-03-07 16:49:59 +0900

    semi first commit

---
+++ semiproject/src/main/java/com/kh/spring14/SemiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiprojectApplication.class, args);
+	}
+
+}
